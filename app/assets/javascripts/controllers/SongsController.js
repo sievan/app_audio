57dd7b2d409cd097f7e65bf1094c19a9b630bd22
Jacob Sievers <jacosiev@gmail.com>
2015-02-02 14:26:34 +0100

    Corrected sorting of songs

--- app/assets/javascripts/controllers/SongsController.js
+++ app/assets/javascripts/controllers/SongsController.js
@@ -7,5 +7,20 @@ Songbook.SongsController = Ember.ArrayController.extend({
     return songs.filter(function(song) {
       return song.get('title').match(rx);
     });
-  }.property('arrangedContent','filter')
+  }.property('arrangedContent','filter'),
+  sortProperties: ['title'],
+  sortAscending: true,
+  sortFunction: function(a, b) {
+    var aVal = a.toLowerCase().charCodeAt(0),
+        bVal = b.toLowerCase().charCodeAt(0);
+    if(aVal === 228) // Make å and ä switch places
+      aVal = 229;
+    else if (aVal === 229)
+      aVal = 228;
+    if(bVal === 228)
+      bVal = 229;
+    else if (bVal === 229)
+      bVal = 228;
+    return aVal-bVal;
+  }
 });
