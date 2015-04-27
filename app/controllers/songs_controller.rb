f91ca427116ea9a18354017ad2840c6be02cf8b3
Jacob Sievers <jacosiev@gmail.com>
2015-04-27 10:50:25 +0200

    Removed unused code

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,8 +2,4 @@ class SongsController < ApplicationController
   def index
     render :json => Song.all
   end
-  def show
-    puts params, params[:id].to_i
-    render :json => {song: all_songs[params[:id].to_i]}
-  end
 end
